5c8a13fd65b27efeaa23300861df2571c30b8b6b
Rahul Khowal <rahulkhowal90@gmail.com>
2024-05-22 19:52:21 +0530

    fixed package name and fixed table design

--- src/main/scala/com/taskmanagement_service/routes/UserRoute.scala
+++ src/main/scala/com/taskmanagement_service/routes/UserRoute.scala
@@ -1,4 +1,4 @@
-package com.taskmanagement_service.Routes
+package com.taskmanagement_service.routes
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives.*
--- src/main/scala/com/taskmanagement_service/utils/RestInterface.scala
+++ src/main/scala/com/taskmanagement_service/utils/RestInterface.scala
@@ -2,7 +2,7 @@ package com.taskmanagement_service.utils
 
 import akka.http.scaladsl.server.Route
 import cats.effect.IO
-import com.taskmanagement_service.Routes.UserRoute
+import com.taskmanagement_service.routes.UserRoute
 import com.taskmanagement_service.business.{UserService, UserServiceImpl}
 import com.taskmanagement_service.database_config.{Configuration, DatabaseConnector}
 import doobie.util.transactor.Transactor
