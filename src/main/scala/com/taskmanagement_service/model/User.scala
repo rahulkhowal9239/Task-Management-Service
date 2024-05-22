bba925f0336044df21b8917cdcef73c2b3b609a6
Rahul Khowal <rahulkhowal90@gmail.com>
2024-05-22 23:36:55 +0530

    create tasked route and write annotations

--- src/main/scala/com/taskmanagement_service/model/User.scala
+++ src/main/scala/com/taskmanagement_service/model/User.scala
@@ -1,6 +1,5 @@
 package com.taskmanagement_service.model
 
-import java.time.LocalDateTime
 
 final case class User(userId: Option[Long] = None, username: String, email: String)
 final case class Status(rowsEffected : Int)
