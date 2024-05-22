fa76bdd7747e250b8740d95087bebbdb32b1e9cc
Rahul Khowal <rahulkhowal90@gmail.com>
2024-05-22 23:37:57 +0530

    create tasked route and write annotations

--- src/main/scala/com/taskmanagement_service/model/User.scala
+++ src/main/scala/com/taskmanagement_service/model/User.scala
@@ -3,4 +3,4 @@ package com.taskmanagement_service.model
 
 final case class User(userId: Option[Long] = None, username: String, email: String)
 final case class Status(rowsEffected : Int)
-case class Task(userId: Long, taskName: String, description: String, status: Option[String], createdOn: Option[String])
+final case class Task(userId: Long, taskName: String, description: String, status: Option[String], createdOn: Option[String])
